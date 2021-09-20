8a3d30a8579ac2abcb9b89e3ebe529e4d21560bb
maSzilard <maszilard87@gamil.com>
2021-09-20 16:03:51 +0300

    calculateNumberOfFlights updaet

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,9 @@
 function Flights() {
-
+    function calculateNumberOfFlights(passengers, capacity) {
+        return Math.ceil((passengers / capacity)) ;
+    }
+    
+    return {calculateNumberOfFlights};
 }
 
+module.exports = Flights();
